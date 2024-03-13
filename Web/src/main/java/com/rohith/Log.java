5e433f4e7ededc8a5dccfb5275accdc8f4638147
Bathula Rohith <148605251+bathula-rohith@users.noreply.github.com>
2024-03-13 12:08:49 +0530

    Add files via upload

---
+++ Web/src/main/java/com/rohith/Log.java
@@ -0,0 +1,59 @@
+
+package com.rohith;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.*;
+import java.sql.*;
+import java.io.*;
+
+public class Log extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res)
+	{
+
+		try {
+
+
+			String n = req.getParameter("na");
+			String p=req.getParameter("pas");
+			
+			res.setContentType("text/html");
+		;
+			
+			String driver ="com.mysql.cj.jdbc.Driver";
+			String url ="jdbc:mysql://localhost:3306/rohith";
+			String uname ="root";
+			String pwd="root";
+			Class.forName(driver);
+			Connection con =DriverManager.getConnection(url, uname, pwd);
+			String qry = "select * from rohith where name=? and password=?";
+			PreparedStatement str =con.prepareStatement(qry);
+			str.setString(1, n);
+			str.setString(2,p);
+	         ResultSet rest = str.executeQuery();
+	         PrintWriter out =res.getWriter();
+			if( rest.next())
+			{
+				RequestDispatcher rd =req.getRequestDispatcher("/profile.jsp");
+				rd.include(req, res);
+				
+			}
+			else
+			{
+				out.println("<h3 style='color:red'> your details is not matched</h3>");
+				RequestDispatcher rd = req.getRequestDispatcher("/Register.html");
+				rd.include(req, res);
+			}
+			
+			
+
+
+		
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+
+}
